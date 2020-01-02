8f42df5d3314fbf33f3346094f6d26aa813e0631
fenglin.zhang@live.cn <fenglin.zhang@live.cn>
2020-01-02 23:35:15 +0800

    完成界面功能的实现。

--- VideoPlayerForWpf/Controls/MonitorUnit.xaml.cs
+++ VideoPlayerForWpf/Controls/MonitorUnit.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,6 +13,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using VideoPlayerForWpf.Controls.Events;
+using VideoPlayerForWpf.Models;
 
 namespace VideoPlayerForWpf.Controls
 {
@@ -20,14 +23,71 @@ namespace VideoPlayerForWpf.Controls
     /// </summary>
     public partial class MonitorUnit : UserControl
     {
+        #region Fields
+
+        private MonitorEntity monitorData;
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public MonitorEntity MonitorData
+        {
+            get { return this.monitorData; }
+            set
+            {
+                this.monitorData = value;
+
+                if (value != null)
+                {
+                    this.tbTitle.Text = $"{value.Name} {value.Code} {value.Address}";
+                }
+                else
+                {
+                    this.tbTitle.Text = string.Empty;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 加载方式
+        /// </summary>
         public MediaState LoadedBehavior
         {
             get { return (MediaState)GetValue(LoadedBehaviorProperty); }
             set { SetValue(LoadedBehaviorProperty, value); }
         }
 
-        // Using a DependencyProperty as the backing store for LoadedBehavior.  This enables animation, styling, binding, etc...
+        /// <summary>
+        /// 表格
+        /// </summary>
+        public int GridRowSpan { get; private set; }
+
+        public int GridColumnSpan { get; private set; }
+
+        public int GridRowIndex { get; private set; }
+
+        public int GridColumnIndex { get; private set; }
+
+        #endregion
+
+        #region Dependency Properties
+
+        /// <summary>
+        /// 注册加载方式依赖属性
+        /// </summary>
         public static readonly DependencyProperty LoadedBehaviorProperty = DependencyProperty.Register("LoadedBehavior", typeof(MediaState), typeof(MonitorUnit), new PropertyMetadata());
+
+        #endregion
+
+        #region Events
+
+        public event EventHandler<ZoomChangedEventArgs> ZoomChanged;
+
+        #endregion
 
         #region Constructors
 
@@ -40,5 +100,66 @@ namespace VideoPlayerForWpf.Controls
         }
 
         #endregion
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="column"></param>
+        /// <param name="row"></param>
+        /// <param name="columnSpan"></param>
+        /// <param name="rowSpan"></param>
+        public void Initialize(int column, int row = 0, int columnSpan = 1, int rowSpan = 1)
+        {
+            this.SetValue(Grid.RowProperty, row);
+            this.SetValue(Grid.ColumnProperty, column);
+
+            if (columnSpan > 0)
+            {
+                this.SetValue(Grid.ColumnSpanProperty, columnSpan);
+            }
+
+            if (rowSpan > 0)
+            {
+                this.SetValue(Grid.RowSpanProperty, rowSpan);
+            }
+
+            this.GridRowIndex = row;
+            this.GridColumnIndex = column;
+
+            this.GridRowSpan = rowSpan;
+            this.GridColumnSpan = columnSpan;
+        }
+
+        public void ShowFullScreen(Grid parentContainer)
+        {
+            this.SetValue(Grid.RowProperty, 0);
+            this.SetValue(Grid.ColumnProperty, 0);
+            this.SetValue(Grid.RowSpanProperty, parentContainer.RowDefinitions.Count);
+            this.SetValue(Grid.ColumnSpanProperty, parentContainer.ColumnDefinitions.Count);
+
+            this.SetValue(Panel.ZIndexProperty, 1000);
+
+            if (this.LoadedBehavior != MediaState.Play)
+            {
+                this.mePlayer.Play();
+            }
+        }
+
+        /// <summary>
+        /// 还原
+        /// </summary>
+        public void Restore()
+        {
+            this.tbZoom.IsChecked = false;
+            this.Initialize(this.GridColumnIndex, this.GridRowIndex, this.GridColumnSpan, this.GridRowSpan);
+        }
+
+        private void ToggleButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is ToggleButton tbn)
+            {
+                this.ZoomChanged?.Invoke(this, new ZoomChangedEventArgs { IsFullScreen = tbn.IsChecked ?? false });
+            }
+        }
     }
 }
---
+++ VideoPlayerForWpf/Service/MonitorDatasService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using VideoPlayerForWpf.Models;
+
+namespace VideoPlayerForWpf.Service
+{
+    public class MonitorDatasService
+    {
+
+        public IList<MonitorEntity> LoadMonitorDatas()
+        {
+            var path = Path.Combine(Environment.CurrentDirectory, "Resources", "navs.xml");
+
+            if (!File.Exists(path))
+            {
+                return new List<MonitorEntity>();
+            }
+
+            return (from c in XDocument.Load(path).Descendants("item")
+                    select new MonitorEntity
+                    {
+                        RowIndex = int.Parse(c.Attribute("num").Value),
+                        Name = c.Attribute("name").Value,
+                        Code = c.Attribute("code").Value,
+                        Address = c.Attribute("area").Value
+                    }).ToList();
+        }
+    }
+}
